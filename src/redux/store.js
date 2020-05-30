b23c150d9b3eb58cbce8018bc32cad1947b4bea9
shivakant shukla <shivakantshukla55@gmail.com>
2020-05-30 15:38:53 +0530

    removed datareducer file

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,7 +2,6 @@ import { createStore, combineReducers, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
 
 import userReducer from "./reducers/userReducer";
-import dataReducer from "./reducers/dataReducer";
 import uiReducer from "./reducers/uiReducer";
 
 const initialState = {};
@@ -11,7 +10,6 @@ const middleware = [thunk];
 
 const reducers = combineReducers({
   user: userReducer,
-  data: dataReducer,
   UI: uiReducer
 });
 
